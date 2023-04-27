1db271a3f620c78175ec0f5334d3e2996ddc51cf
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2023-04-27 15:43:41 +0200

    JavaDockyDashboard work on

--- src/main/java/com/tecknobit/javadocky/JavaDockyDashboard.java
+++ src/main/java/com/tecknobit/javadocky/JavaDockyDashboard.java
@@ -12,6 +12,7 @@ import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.psi.*;
 import com.intellij.ui.EditorTextField;
+import com.intellij.ui.components.JBScrollPane;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.table.JBTable;
 import org.jdesktop.swingx.HorizontalLayout;
@@ -29,53 +30,116 @@ import java.util.prefs.Preferences;
 
 import static com.intellij.ui.content.ContentFactory.SERVICE.getInstance;
 import static com.intellij.util.ui.JBUI.Borders.empty;
-import static com.tecknobit.javadocky.JavaDockyDashboard.Tag.instance;
-import static com.tecknobit.javadocky.JavaDockyDashboard.Tag.returnType;
+import static com.tecknobit.javadocky.JavaDockyDashboard.Tag.*;
 import static java.awt.Color.getColor;
-import static java.awt.Font.DIALOG;
-import static java.awt.Font.PLAIN;
+import static java.awt.Font.*;
 import static javax.swing.BorderFactory.createLineBorder;
 import static javax.swing.SwingConstants.NORTH;
 import static javax.swing.SwingConstants.SOUTH;
 
+/**
+ * The {@code JavaDockyDashboard} class is useful to manage the {@code JavaDocky}'s configuration and set all the
+ * docu-string templates to use
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ * @see ToolWindowFactory
+ **/
 @Service
 public class JavaDockyDashboard implements ToolWindowFactory {
 
+    // TODO: 27/04/2023 add PARAMS tag to insert in the methods or constructors to manage the params of the statement
+    //  if insert or not if they has params or not
+
+    /**
+     * {@code Tag} list of available tags to use to give directions to {@code JavaDocky}
+     **/
     public enum Tag {
 
+        /**
+         * {@code className} tag -> use to fetch the name of the class
+         **/
+        className("className"),
+
+        /**
+         * {@code instance} tag -> use to link to an instance of the class
+         **/
         instance("instance"),
+
+        /**
+         * {@code returnType} tag -> use to fetch the return type of method
+         **/
         returnType("returnType");
 
+        /**
+         * {@code tag} value
+         **/
         private final String tag;
 
+        /**
+         * Constructor to init {@link Tag}
+         *
+         * @param tag: tag value
+         **/
         Tag(String tag) {
             this.tag = "<" + tag + ">";
         }
 
+        /**
+         * Method to get {@link #tag} instance <br>
+         * No-any params required
+         *
+         * @return {@link #tag} instance as {@link String}
+         **/
         public String getTag() {
             return tag;
         }
 
     }
 
+    /**
+     * {@code preferences} useful to manage the data stored by this plugin
+     **/
     private static final Preferences preferences = Preferences.userRoot().node("/user/javadocky");
 
+    /**
+     * Method to create the toolwindow to insert the plugin UI
+     *
+     * @param project:    the current project
+     * @param toolWindow: toolwindow where set the content
+     **/
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         try {
             JavaDockyContent javaDockyContent = new JavaDockyContent(project);
-            Content content = getInstance().createContent(javaDockyContent.getContentPanel(), "", false);
+            Content content = getInstance().createContent(javaDockyContent.getContent(), "", false);
             toolWindow.getContentManager().addContent(content);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
+    /**
+     * The {@code JavaDockyContent} class is useful to manage the plugin's UI content and manage the dashboard
+     *
+     * @author N7ghtm4r3 - Tecknobit
+     **/
     private static class JavaDockyContent {
 
+        /**
+         * {@code items} available to create the docu-strings templates
+         **/
         private static final String[] items = new String[]{"Classes", "Fields", "Constructors", "Methods"};
+
+        /**
+         * {@code contentPanel} main panel to contain the ui
+         **/
         private final JPanel contentPanel = new JPanel();
 
+        /**
+         * Constructor to init {@link JavaDockyContent}
+         *
+         * @param project: the current project
+         **/
         public JavaDockyContent(Project project) throws Exception {
             contentPanel.setLayout(new VerticalLayout(10));
             contentPanel.setBorder(empty(10));
@@ -85,35 +149,52 @@ public class JavaDockyDashboard implements ToolWindowFactory {
             setConfigurationLayout(project);
         }
 
+        /**
+         * Method to get a header title
+         *
+         * @param title: title of the label
+         * @return header title as {@link JLabel}
+         **/
         private JLabel getHeaderTitle(String title) {
             JLabel lTitle = new JLabel(title);
-            lTitle.setFont(new Font(DIALOG, PLAIN, 20));
+            lTitle.setFont(getFontText(20));
             return lTitle;
         }
 
+        /**
+         * Method to set the tags layout <br>
+         * No-any params required
+         **/
         private void setTagsLayout() {
             final HashMap<Tag, String> descriptions = new HashMap<>();
+            descriptions.put(className, "Fetch the name of the class");
             descriptions.put(instance, "Link to an instance of the class");
-            descriptions.put(returnType, "Fetch the return type of a method");
+            descriptions.put(returnType, "Fetch the return type of method");
             JBTable table = new JBTable();
-            table.setBorder(createLineBorder(getColor("#f5f5f5"), 1));
-            table.setFont(new Font(DIALOG, PLAIN, 15));
+            table.setDefaultEditor(Object.class, null);
+            table.setRowSelectionAllowed(false);
+            table.setCellSelectionEnabled(true);
+            table.setFont(getFontText(SANS_SERIF, 15));
             DefaultTableModel model = new DefaultTableModel(new String[]{"Tag", "Description"}, 0);
-            model.addRow(new Object[]{"Tag", "Description"});
             for (Tag tag : Tag.values())
                 model.addRow(new Object[]{tag.getTag(), descriptions.get(tag)});
             table.setModel(model);
-            contentPanel.add(table);
+            contentPanel.add(new JBScrollPane(table));
         }
 
+        /**
+         * Method to set the configuration items layout
+         *
+         * @param project: the current project
+         **/
         private void setConfigurationLayout(Project project) throws Exception {
             for (String item : items) {
-                JPanel container = new JPanel(new GridLayout(1, 1));
+                JPanel container = new JPanel(new VerticalLayout());
                 container.setBorder(createLineBorder(getColor("#f5f5f5"), 1));
                 JPanel itemPanel = new JPanel(new HorizontalLayout(50));
                 itemPanel.setBorder(empty(10));
                 JCheckBox itemCheckBox = new JCheckBox(item);
-                itemCheckBox.setFont(new Font(DIALOG, PLAIN, 15));
+                itemCheckBox.setFont(getFontText(15));
                 itemCheckBox.setSelected(preferences.get(item, null) != null);
                 itemPanel.add(itemCheckBox);
                 Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
@@ -127,11 +208,14 @@ public class JavaDockyDashboard implements ToolWindowFactory {
                         .createExpressionCodeFragment("", element, null, true);
                 Document document = PsiDocumentManager.getInstance(project).getDocument(code);
                 EditorTextField docuText = new EditorTextField(document, editor.getProject(), JavaFileType.INSTANCE);
-                docuText.setFont(new Font(DIALOG, PLAIN, 13));
+                docuText.setFont(getFontText(13));
                 docuText.setPreferredWidth(300);
                 docuText.setOneLineMode(false);
                 docuText.setVisible(false);
                 BasicArrowButton arrowButton = new BasicArrowButton(SOUTH) {
+                    /**
+                     * {@inheritDoc}
+                     **/
                     @Override
                     public Dimension getPreferredSize() {
                         return new Dimension(25, 25);
@@ -140,18 +224,24 @@ public class JavaDockyDashboard implements ToolWindowFactory {
                 arrowButton.setVisible(itemCheckBox.isSelected());
                 arrowButton.setBorder(createLineBorder(new Color(0f, 0f, 0f, 0f)));
                 arrowButton.addActionListener(new ActionListener() {
+                    /**
+                     * {@inheritDoc}
+                     **/
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         if (arrowButton.getDirection() == NORTH)
-                            setPanelLayout(arrowButton, docuText, container, SOUTH, false, 1, item);
+                            setPanelLayout(arrowButton, docuText, SOUTH, false, item);
                         else
-                            setPanelLayout(arrowButton, docuText, container, NORTH, true, 2, item);
+                            setPanelLayout(arrowButton, docuText, NORTH, true, item);
                     }
                 });
                 itemCheckBox.addActionListener(new ActionListener() {
+                    /**
+                     * {@inheritDoc}
+                     **/
                     @Override
                     public void actionPerformed(ActionEvent e) {
-                        setPanelLayout(arrowButton, docuText, container, SOUTH, false, 1, item);
+                        setPanelLayout(arrowButton, docuText, SOUTH, false, item);
                         boolean isSelected = itemCheckBox.isSelected();
                         arrowButton.setVisible(isSelected);
                         if (!isSelected)
@@ -167,7 +257,9 @@ public class JavaDockyDashboard implements ToolWindowFactory {
                     @Override
                     public void documentChanged(@NotNull DocumentEvent event) {
                         DocumentListener.super.documentChanged(event);
-                        preferences.put(item, docuText.getText());
+                        String vDocu = docuText.getText();
+                        if (vDocu.startsWith("/**") && vDocu.endsWith("**/"))
+                            preferences.put(item, vDocu);
                     }
                 });
                 itemPanel.add(arrowButton);
@@ -177,16 +269,51 @@ public class JavaDockyDashboard implements ToolWindowFactory {
             }
         }
 
-        private void setPanelLayout(BasicArrowButton arrowButton, EditorTextField docuText, JPanel container,
-                                    int direction, boolean isVisible, int rows, String item) {
-            container.setLayout(new GridLayout(rows, 1));
+        /**
+         * Method to get the font for a {@link JComponent}
+         *
+         * @param size: size of the text
+         * @return font as {@link Font}
+         **/
+        private Font getFontText(int size) {
+            return getFontText(DIALOG, size);
+        }
+
+        /**
+         * Method to get the font for a {@link JComponent}
+         *
+         * @param font: font to use
+         * @param size: size of the text
+         * @return font as {@link Font}
+         **/
+        private Font getFontText(String font, int size) {
+            return new Font(font, PLAIN, size);
+        }
+
+        /**
+         * Method to set the panel layout
+         *
+         * @param arrowButton: arrow button to work on
+         * @param docuText:    the editor where insert the template
+         * @param direction:   the direction of the arrow button
+         * @param isVisible:   whether the arrow button is visible
+         * @param item:        the item of the panel
+         **/
+        private void setPanelLayout(BasicArrowButton arrowButton, EditorTextField docuText, int direction,
+                                    boolean isVisible, String item) {
             arrowButton.setDirection(direction);
             docuText.setVisible(isVisible);
             docuText.setText(preferences.get(item, "/**\n *\n **/"));
         }
 
-        public JPanel getContentPanel() {
-            return contentPanel;
+        /**
+         * Method to get {@link #contentPanel} instance <br>
+         * No-any params required
+         *
+         * @return {@link #contentPanel} instance as {@link JBScrollPane} to make it scrollable
+         **/
+        public JBScrollPane getContent() {
+            return new JBScrollPane(contentPanel);
         }
 
     }
