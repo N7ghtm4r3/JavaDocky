dbbf18209116f17a3d83faf42e3a48bb45622b1a
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2023-07-17 19:21:05 +0200

    Work on auto-documentation params

--- src/main/java/com/tecknobit/javadocky/listeners/DocumentationChangeListener.java
+++ src/main/java/com/tecknobit/javadocky/listeners/DocumentationChangeListener.java
@@ -1,13 +1,14 @@
 package com.tecknobit.javadocky.listeners;
 
-import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.project.DumbService;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.newvfs.BulkFileListener;
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
 import com.intellij.psi.*;
 import com.intellij.psi.javadoc.PsiDocComment;
+import com.tecknobit.javadocky.JavaDockyConfiguration;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -19,22 +20,59 @@ import static com.tecknobit.javadocky.JavaDockyConfiguration.MethodType.SETTER;
 import static com.tecknobit.javadocky.JavaDockyConfiguration.MethodType.reachMethodType;
 import static com.tecknobit.javadocky.JavaDockyDocuManager.formatFieldTemplate;
 
+/**
+ * The {@code DocumentationChangeListener} class is useful to detect when the current class where the user is working on
+ * changes the documentation of the fields, this allows this listener to modify all the occurrences of that field of all
+ * documentation comments that appear in the class
+ *
+ * @author N7ghtm4r3 - Tecknobit
+ * @apiNote this when the {@link JavaDockyConfiguration.Tag#params} is used in the configured template
+ * @see BulkFileListener
+ */
 public class DocumentationChangeListener implements BulkFileListener {
 
+    /**
+     * {@code currentDocus} list of the current documentation comments of the class
+     * @apiNote when the class changes will be recreated
+     */
     private static ArrayList<String> currentDocus = new ArrayList<>();
 
+    /**
+     * {@code tempDocus} check list of the current documentation comments of the class
+     * @apiNote when the class changes will be recreated
+     */
     private static ArrayList<String> tempDocus = new ArrayList<>();
 
+    /**
+     * {@code OPEN_CURLY_BRACKET_REGEX} regex for the "{" character
+     */
     private static final String OPEN_CURLY_BRACKET_REGEX = "OCBR";
 
+    /**
+     * {@code OPEN_CURLY_BRACKET_REGEX} regex for the "}" character
+     */
     private static final String CLOSE_CURLY_BRACKET_REGEX = "CCBR";
 
+    /**
+     * {@code OPEN_CURLY_BRACKET_REGEX} regex for the "*" character
+     */
     private static final String STAR_REGEX = "STRX";
 
+    /**
+     * {@code listenerProject} instance of the project to work on
+     */
     public static Project listenerProject;
 
+    /**
+     * {@code currentClass} instance of the current class to work on
+     */
     private PsiClass currentClass;
 
+    /**
+     * Method to compute after an event happened
+     *
+     * @param events: list of events happened
+     */
     @Override
     public void after(@NotNull List<? extends @NotNull VFileEvent> events) {
         BulkFileListener.super.after(events);
@@ -57,10 +95,17 @@ public class DocumentationChangeListener implements BulkFileListener {
         }
     }
 
+    /**
+     * Method to rewrite all the documentation comments of the field (or multiple fields at the same time) where the
+     * documentation has been changed
+     *
+     * @param sourceClass: class where rewrite the documentation
+     */
     private void rewriteDocu(PsiClass sourceClass) {
-        PsiClass tmpClass = PsiElementFactory.getInstance(listenerProject).createClassFromText(
-                insertDanglingMetaCharacters(getChanges(sourceClass, sourceClass.getText())), null).getInnerClasses()[0];
         if (execute(currentClass)) {
+            PsiClass tmpClass = PsiElementFactory.getInstance(listenerProject).createClassFromText(
+                    insertDanglingMetaCharacters(getChanges(sourceClass, sourceClass.getText())),
+                    null).getInnerClasses()[0];
             for (PsiMethod tmpMethod : tmpClass.getMethods()) {
                 boolean isSetter = reachMethodType(tmpMethod) == SETTER;
                 if (tmpMethod.getName().equals(tmpClass.getName()) || isSetter) {
@@ -71,33 +116,46 @@ public class DocumentationChangeListener implements BulkFileListener {
                                 PsiDocComment tmpMethodDocComment = tmpMethod.getDocComment();
                                 if ((methodDocComment != null && tmpMethodDocComment != null)
                                         && (!methodDocComment.getText().equals(tmpMethodDocComment.getText()))) {
-                                    ApplicationManager.getApplication().invokeLater(() ->
-                                            WriteCommandAction.runWriteCommandAction(listenerProject, () -> {
-                                                PsiDocumentManager.getInstance(listenerProject).commitAllDocuments();
-                                                methodDocComment.replace(tmpMethodDocComment);
-                                            })
-                                    );
+                                    DumbService.getInstance(listenerProject).smartInvokeLater(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            WriteCommandAction.runWriteCommandAction(listenerProject, new Runnable() {
+                                                @Override
+                                                public void run() {
+                                                    methodDocComment.replace(tmpMethodDocComment);
+                                                }
+                                            });
+                                        }
+                                    });
                                 }
                             }
                         }
                     }
                 }
             }
-        }
-        for (PsiClass innerClass : sourceClass.getInnerClasses())
-            rewriteDocu(innerClass);
-    }
-
-    private boolean execute(PsiClass sourceClass) {
-        populateTempDocus(sourceClass);
-        boolean execute = !Arrays.deepEquals(currentDocus.toArray(), tempDocus.toArray());
-        if (execute) {
+            for (PsiClass innerClass : sourceClass.getInnerClasses())
+                rewriteDocu(innerClass);
             currentDocus = new ArrayList<>(new ArrayList<>(tempDocus));
             tempDocus = new ArrayList<>();
         }
-        return execute;
     }
 
+    /**
+     * Method to check if the rewrite of the documentation must execute
+     *
+     * @param sourceClass: the class where check if execute the rewrite of the documentation
+     * @return whether the rewrite of the documentation must execute
+     */
+    private boolean execute(PsiClass sourceClass) {
+        populateTempDocus(sourceClass);
+        return !Arrays.deepEquals(currentDocus.toArray(), tempDocus.toArray());
+    }
+
+    /**
+     * Method to assemble the {@link #tempDocus}
+     *
+     * @param sourceClass: the class where fetch the documentation comments of the fields
+     */
     private void populateTempDocus(PsiClass sourceClass) {
         for (PsiField field : sourceClass.getFields()) {
             PsiDocComment docComment = field.getDocComment();
@@ -111,8 +169,13 @@ public class DocumentationChangeListener implements BulkFileListener {
             populateTempDocus(innerClass);
     }
 
-    private String getChanges(PsiClass psiClass, String changes) {
-        for (PsiField field : psiClass.getFields()) {
+    /**
+     * Method to get if the documentation of the fields of the class has been changed
+     *
+     * @param sourceClass: the class from fetch the fields and their documentation comment
+     */
+    private String getChanges(PsiClass sourceClass, String changes) {
+        for (PsiField field : sourceClass.getFields()) {
             String fieldName = field.getName();
             PsiDocComment docComment = field.getDocComment();
             if (docComment != null) {
@@ -127,11 +190,16 @@ public class DocumentationChangeListener implements BulkFileListener {
                 }
             }
         }
-        for (PsiClass innerClass : psiClass.getInnerClasses())
-            return getChanges(innerClass, changes);
         return changes;
     }
 
+    /**
+     * Method from fetch the regex to use for rewrite the documentation
+     *
+     * @param classText: the text of the current class
+     * @param fieldName: the field name from fetch the regex
+     * @return the regex to rewrite the new documentation comment of the field inserted
+     */
     private String getRegex(String classText, String fieldName) {
         String regex = "* @param " + fieldName + ": ";
         for (String line : insertDanglingMetaCharacters(classText).split("\n")) {
@@ -143,12 +211,30 @@ public class DocumentationChangeListener implements BulkFileListener {
         return null;
     }
 
+    /**
+     * Method to remove the dangling meta characters from the text inserted
+     *
+     * @param text: the text from remove the dangling meta characters
+     * @return text without the dangling meta characters as {@link String}
+     * @apiNote this method is useful during the replace operation on a {@link String} to obtain a clean value to use,
+     * in this method are used the: {@link #OPEN_CURLY_BRACKET_REGEX}, {@link #CLOSE_CURLY_BRACKET_REGEX} and
+     * {@link #STAR_REGEX} regex
+     */
     private String removeDanglingMetaCharacters(String text) {
         return text.replaceAll("\\{", OPEN_CURLY_BRACKET_REGEX)
                 .replaceAll("}", CLOSE_CURLY_BRACKET_REGEX)
                 .replaceAll("\\*", STAR_REGEX);
     }
 
+    /**
+     * Method to insert the dangling meta characters from the text inserted
+     *
+     * @param text: the text where insert the dangling meta characters
+     * @return text with the dangling meta characters as {@link String}
+     * @apiNote this method is useful during the replace operation on a {@link String} to obtain a clean value to use in
+     * the correct way, in this method are used the: {@link #OPEN_CURLY_BRACKET_REGEX}, {@link #CLOSE_CURLY_BRACKET_REGEX}
+     * and {@link #STAR_REGEX} regex
+     */
     private String insertDanglingMetaCharacters(String text) {
         return text.replaceAll(OPEN_CURLY_BRACKET_REGEX, "{")
                 .replaceAll(CLOSE_CURLY_BRACKET_REGEX, "}")
